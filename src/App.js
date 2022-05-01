402113fb77263424c928a2fab18857de587301f3
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-05-01 12:06:26 +0600

    Project Work Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Inventories from './pages/Inventories/Inventories';
+import Home from './pages/Home/Home';
+import Blogs from './pages/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To My Books House</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/inventories' element={<Inventories></Inventories>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>If You Want to Contact with me, Follow the informations</h1>
+        </div>
+    );
+};
+
+export default Contact;
